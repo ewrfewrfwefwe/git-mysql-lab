2f2359479f866179807de5c7c39dd5921add4a14
CZ <2462406009@stu.suda.edu.cn>
2026-03-20 14:24:34 +0800

    feat: 增加 email 字段

--- init.sql
+++ init.sql
@@ -11,3 +11,14 @@ CREATE TABLE IF NOT EXISTS student (
 
 -- 插入一条初始数据
 INSERT INTO student (name, age) VALUES ('Alice', 20);
+
+USE student_db;
+-- 给学生表增加一个邮箱列
+-- 切换数据库
+USE student_db;
+
+-- 增加一个邮箱字段
+ALTER TABLE students ADD COLUMN email VARCHAR(100);
+
+-- 给 Tom 更新一个邮箱试试
+UPDATE students SET email = 'tom@example.com' WHERE name = 'Tom';
